48956b3807c5a1de635dc6a701a32d033fa1f93e
jtagaca12 <115109184+jtagaca12@users.noreply.github.com>
2022-12-26 16:41:49 -0800

    Create hey.py

---
+++ hey.py
@@ -0,0 +1,11 @@
+#From a minimum (inclusive) to maximum (exclusive).
+#Minimum is 0 unless otherwise specified
+
+for i in range(4)
+	print(i) #0, 1, 2, 3
+  
+for i in range(2, 4)
+	print(i) #2, 3
+  
+for i in range(1, 7, 2):
+  print(i) #1, 3, 5
